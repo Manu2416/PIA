db2f6ac58dc8564fd76558b690fda2618daf874d
manu <manuelgm6@hotmail.com>
2025-11-17 18:56:06 +0100

    ejercicio-evaluacion

--- ej1.py
+++ ej1.py
@@ -1,11 +1,25 @@
-Diccionario = {}
+import csv
 
-while (True):
-    Palabra = input("dame una palabra para traducir")
+def procesar_ventas(fichero,productos_buscar):
+    listas = []
+    cantidad_productos = 0
+    mismo_producto = 0
+    with open(fichero,"r",encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            listas.append(row)
 
-    ListaPalabras = Palabra.split(";")
+    for lista in listas:
+        for i in range(len(lista)):
+            if lista[i] == productos_buscar:
+                    mismo_producto += 1
+                    cantidad_productos += int(lista[1])
+                    beneficio = cantidad_productos * int(lista[2])
+                    return ( f" el producto {productos_buscar} ha obtenido un beneficio de {beneficio} {mismo_producto}")
+            else:
+                 return ("No existe ese productos")
+                    
+        
+    
 
-    for i in range(ListaPalabras):
-        Diccionario[i] = ListaPalabras(i+1)
-
-print (Diccionario)
+procesar_ventas("./docsEJ/productos.csv","hig")
